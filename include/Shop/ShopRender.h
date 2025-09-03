773e483e5087b7033a788ac393410fa0a3fd52fc
klcetines <laneulaestubular@gmail.com>
2025-09-03 14:20:08 +0100

    ShopItems as Cards
    Rendered Shop items as a cards, added description, cost and name, but sprites are missing

--- include/Shop/ShopRender.h
+++ include/Shop/ShopRender.h
@@ -5,5 +5,21 @@ namespace ShopRender {
     void render(Shop& shop, sf::RenderWindow& window, sf::Font& font);
     void renderTitle(Shop& shop, sf::RenderWindow& window, sf::Font& font);
     void renderShopItems(Shop& shop, sf::RenderWindow& window, sf::Font& font);
+    void renderCard(sf::RenderWindow& window, const sf::Texture& texture, float xPos, float yPos, 
+                 float width, float height, bool isSelected);
+    void renderItemNumber(sf::RenderWindow& window, sf::Font& font, int number, float xPos, float yPos);
+    void renderItemDescription(sf::RenderWindow& window, sf::Font& font, const std::string& description, 
+                           float xPos, float yPos);
+
+    string wrapText(const string& text, unsigned int maxLength);
+    void renderItemName(sf::RenderWindow& window, sf::Font& font, const std::string& name, 
+                     float xPos, float yPos, float cardWidth);
+    void renderItemCost(sf::RenderWindow& window, sf::Font& font, int cost, 
+                     float xPos, float yPos, float cardWidth);
     void renderCurrency(Shop& shop, sf::RenderWindow& window, sf::Font& font);
+
+
+    static float currentOffset = 0.f;
+
+
 }
--- src/Shop/ShopRender.cpp
+++ src/Shop/ShopRender.cpp
@@ -16,19 +16,112 @@ void ShopRender::renderTitle(Shop& shop, RenderWindow& window, Font& font) {
     window.draw(title);
 }
 
+void ShopRender::renderCard(RenderWindow& window, const Texture& texture, float xPos, float yPos, 
+                         float width, float height, bool isSelected) {
+    Sprite cardSprite(texture);
+    cardSprite.setPosition(xPos, yPos);
+    cardSprite.setScale(
+        width / cardSprite.getLocalBounds().width,
+        height / cardSprite.getLocalBounds().height
+    );
+    cardSprite.setColor(isSelected ? Color(150, 255, 150) : Color::White);
+    window.draw(cardSprite);
+}
+
 void ShopRender::renderShopItems(Shop& shop, RenderWindow& window, Font& font) {
     const auto& showcase = shop.getShowcaseItems();
-    for (int i = 0; i < shop.getMaxShopItems(); ++i) {
-        Text itemText(showcase[i].name + " (" + to_string(showcase[i].cost) + ")", font, 24);
-        itemText.setPosition(50, 80 + i * 40);
-        itemText.setFillColor(i == shop.getSelectedIndex() ? Color::Green : Color::White);
-        window.draw(itemText);
-
-        Text desc(showcase[i].description, font, 16);
-        desc.setPosition(300, 85 + i * 40);
-        desc.setFillColor(Color(180, 180, 180));
-        window.draw(desc);
+    Texture cardTexture;
+    if (!cardTexture.loadFromFile("assets/sprites/Cards/atac.png")) {
+        return;
     }
+
+    const float cardWidth = 250.f;
+    const float cardHeight = 350.f;
+    const float cardSpacing = 30.f;
+    const pair<float, float> windowSize = { 
+            static_cast<float>(window.getSize().x),
+            static_cast<float>(window.getSize().y) 
+        };
+
+    const float startY = (windowSize.first - cardHeight) / 2.f;
+
+    const int selectedIndex = shop.getSelectedIndex();
+    const float targetOffset = -(selectedIndex * (cardWidth + cardSpacing)) + (windowSize.second / 2.f - cardWidth / 2.f);
+
+    const float lerpFactor = 0.1f;
+    currentOffset = currentOffset + (targetOffset - currentOffset) * lerpFactor;
+
+    for (int i = 0; i < shop.getMaxShopItems(); ++i) {
+        float xPos = (cardWidth + cardSpacing) * i + currentOffset;
+        
+        if (xPos + cardWidth < -100 || xPos > windowSize.second + 100) {
+            continue;
+        }
+
+        renderCard(window, cardTexture, xPos, startY, cardWidth, cardHeight, i == selectedIndex);
+        renderItemName(window, font, showcase[i].name, xPos, startY + 70, cardWidth);
+        renderItemCost(window, font, showcase[i].cost, xPos + 55, startY + 25, cardWidth);
+        renderItemDescription(window, font, showcase[i].description, xPos, startY + 255);
+    }
+}
+
+void ShopRender::renderItemDescription(RenderWindow& window, Font& font, const string& description, 
+                                   float xPos, float yPos) {
+    string wrappedText = wrapText(description, 30);
+    
+    Text desc(wrappedText, font, 15);  
+    desc.setPosition(xPos + 20, yPos);
+    desc.setScale(0.9f, 0.9f);
+    desc.setFillColor(Color::Black);
+    window.draw(desc);
+}
+
+string ShopRender::wrapText(const string& text, unsigned int maxLength) {
+    string result;
+    string currentLine;
+    string currentWord;
+
+    for (char c : text) {
+        if (c == ' ') {
+            if (currentLine.length() + currentWord.length() > maxLength && !currentLine.empty()) {
+                result += currentLine + '\n';
+                currentLine = currentWord + ' ';
+                currentWord = "";
+            } else {
+                currentLine += currentWord + ' ';
+                currentWord = "";
+            }
+        } else {
+            currentWord += c;
+        }
+    }
+
+    // Handle the last word
+    if (currentLine.length() + currentWord.length() > maxLength && !currentLine.empty()) {
+        result += currentLine + '\n' + currentWord;
+    } else {
+        result += currentLine + currentWord;
+    }
+
+    return result;
+}
+
+void ShopRender::renderItemName(RenderWindow& window, Font& font, const string& name, 
+                            float xPos, float yPos, float cardWidth) {
+    Text itemText(name, font, 24);
+    float textX = xPos + (cardWidth - itemText.getLocalBounds().width) / 2;
+    itemText.setPosition(textX, yPos);
+    itemText.setFillColor(Color::Black);
+    window.draw(itemText);
+}
+
+void ShopRender::renderItemCost(RenderWindow& window, Font& font, int cost, 
+                            float xPos, float yPos, float cardWidth) {
+    Text costText(to_string(cost), font, 22);
+    float textX = xPos + (cardWidth - costText.getLocalBounds().width) / 2;
+    costText.setPosition(textX, yPos);
+    costText.setFillColor(Color::Black);
+    window.draw(costText);
 }
 
 void ShopRender::renderCurrency(Shop& shop, RenderWindow& window, Font& font) {
